da0a72411596ce312e15eb95ab454d526ce7b8d7
Kevin Newcombe <kevinjnewcombe@gmail.com>
2018-08-18 16:10:29 -0600

    Stripped out all old/irrelevant code from 3d menu.

--- index.php
+++ index.php
@@ -1,37 +1,14 @@
 <!doctype html>
 <head>
-  <title>three / les paul</title>
+  <title>three / hotspots</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://fonts.googleapis.com/css?family=Raleway:700i,900" rel="stylesheet">
   <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
 </head>
 <body>
-  <div id="hamburger-nav">
-    <div class="center">
-      <span><a href="#guitars" class="current">GUITARS</a></span>
-      <span><a href="#bases">BASES</a></span>
-      <span><a href="#amps">AMPS</a></span>
-      <span><a href="#pedals">PEDALS</a></span>
-      <span><a href="#others">OTHERS</a></span>
-    </div>
-  </div>
-
-  <a id="toggle-nav">
-    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-       width="30px" height="30px" viewBox="0 0 30 30" style="enable-background:new 0 0 30 30;" xml:space="preserve">
-      <rect width="30" height="6"/>
-      <rect y="12" width="30" height="6"/>
-      <rect y="24" width="24" height="6"/>
-    </svg>
-  </a>
-
-  <div id="loading"></div>
-  <p class="credits">Based on a <a href="https://dribbble.com/shots/4773637-3D-flip-menu" target="_blank">concept by Minh Pham</a>. (Click the hamburger nav)</p>
-
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.1/TweenMax.min.js"></script>
   <script type="text/javascript" src="js/three/three.min.js"></script>
   <script type="text/javascript" src="js/three/OBJLoader.js"></script>
   <script type="text/javascript" src="js/three/MTLLoader.js"></script>
-  <script type="text/javascript" src="js/app.js?v=2"></script>
+  <script type="text/javascript" src="js/three/OrbitControls.js"></script>
+  <script type="text/javascript" src="js/app.js?v=<?php echo time(); ?>"></script>
 </body>
 </html>
