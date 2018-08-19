7d2623448b60c5453a0bf4fbd751eefe5fd0e90f
Kevin Newcombe <kevinjnewcombe@gmail.com>
2018-08-18 18:03:57 -0600

    Added alternate models to the reference materials folder.

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
+<?php 
+  if($_SERVER['HTTP_HOST'] == 'localhost:8888'){
+    $v = time();
+  }else{
+    $v = 2;
+  }
+?>
 <!doctype html>
 <head>
   <title>three / hotspots</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+  <link rel="stylesheet" href="css/style.css?v=<?php echo $v; ?>" type="text/css" media="all" />
 </head>
 <body>
+
   <script type="text/javascript" src="js/three/three.min.js"></script>
   <script type="text/javascript" src="js/three/OBJLoader.js"></script>
   <script type="text/javascript" src="js/three/MTLLoader.js"></script>
   <script type="text/javascript" src="js/three/OrbitControls.js"></script>
-  <script type="text/javascript" src="js/app.js?v=<?php echo time(); ?>"></script>
+  <script type="text/javascript" src="js/app.js?v=<?php echo $v; ?>"></script>
 </body>
 </html>
